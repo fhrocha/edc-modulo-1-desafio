0bfec389561ff84ac4d1aa59976e7d49a8b233f6
Fabio <fhrocha@gmail.com>
2022-08-31 18:59:50 -0300

    ajuste de nome de diretorio s3 para state terraform

--- iac/provider.tf
+++ iac/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3"{
     bucket = "terraform-state-422471183879"
-    key = "state/igti=edc/modulo1/desafio/terraform.tfstate"
+    key = "state/igti-edc/modulo1/desafio/terraform.tfstate"
     region = "us-east-1"
   }
 }
