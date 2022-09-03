3f7e5e47e38346e891f9d6ed692a326b0d31a695
Fabio <fhrocha@gmail.com>
2022-09-03 13:35:03 -0300

    emr config

--- iac/emr.tf
+++ iac/emr.tf
@@ -111,6 +111,23 @@ resource "aws_subnet" "main" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id = aws_vpc.main.id
+  route_table_id = aws_route_table.r.id
+}
+
 ###
 
 # IAM Role setups
